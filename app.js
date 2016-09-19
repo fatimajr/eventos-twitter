7fd5883551b7b55de784c498f761db9a26ab8e24
fatima <fatima.jacoboruiz@gmail.com>
2016-09-19 17:53:35 -0500

    estilos css básicos

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ window.addEventListener("load", function(){
 	var boton = document.getElementById("boton");
 	boton.addEventListener ("click", function(e){
 		e.preventDefault();//para que cambie al submit del boton y no recarge la pagina
-		var txt = document.getElementById("texto").value;
-		var hijo2 = document.createElement("p");
-		hijo2.innerText = txt;
-		var imprimir = document.getElementById("imprimir");
-		imprimir.insertBefore(hijo2, imprimir.childNodes[0]);
-		document.getElementById("texto").value = "";
+		var textArea = document.getElementById("texto");
+		agregarMensaje(textArea.value);
+		textArea.value = "";
 	});
+
+	function agregarMensaje(txt){
+		var parrafo = document.createElement("p")
+		parrafo.innerText = txt;
+		var imprimir = document.getElementById("imprimir");
+		imprimir.insertBefore(parrafo, imprimir.childNodes[0]).classList.add("box");;
+	}
 });
-
-
