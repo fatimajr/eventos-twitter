ee39b10a39639521003e7bf9c9e2d6c23f9e1522
fatima <fatima.jacoboruiz@gmail.com>
2016-09-19 16:22:53 -0500

    boton submit - prevent default

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 window.addEventListener("load", function(){
 	var boton = document.getElementById("boton");
-	boton.addEventListener ("click", function(){
+	boton.addEventListener ("click", function(e){
+		e.preventDefault();//para que cambie al submit del boton y no recarge la pagina
 		var txt = document.getElementById("texto").value;
 		var hijo2 = document.createElement("p");
 		hijo2.innerText = txt;
 		var imprimir = document.getElementById("imprimir");
 		imprimir.insertBefore(hijo2, imprimir.childNodes[0]);
 		document.getElementById("texto").value = "";
-})});
+	});
+});
+
+
