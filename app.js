58d2ca9b2a222eb2e148ae7ec46462bb8d3e3c29
fatima <fatima.jacoboruiz@gmail.com>
2016-09-20 15:43:36 -0500

    enter textarea

--- app.js
+++ app.js
@@ -4,25 +4,40 @@ window.addEventListener("load", function(){
 	var textArea = document.getElementById("texto");
 	var contador = document.getElementById("contador");
 	var caracteres = contador.innerHTML = 140; 
-
+	textArea.addEventListener("keydown", autosize);
 	boton.addEventListener ("click", function(e){
 		e.preventDefault();//para que cambie al submit del boton y no recarge la pagina
 		agregarMensaje(textArea.value);
 		textArea.value = "";
-		caracteres = 140;
+		contador.innerHTML = 140;
 		boton.disabled = true;
+		contador.classList.remove("purple");
+		contador.classList.remove("white");
+		resize();
 	});
-
-	function agregarMensaje(txt){
-		var parrafo = document.createElement("p")
-		parrafo.innerText = txt;
-		var imprimir = document.getElementById("imprimir");
-		imprimir.insertBefore(parrafo, imprimir.childNodes[0]).classList.add("box");;
-	}
-
 	textArea.addEventListener("keydown", function(){
 		boton.disabled = false;
-		var longitud = textArea.value.length;
+		var longitud = textArea.value.length+1;
+		cantidadCaracteres(longitud);
+		cambioColor(longitud);
+	});
+	function agregarMensaje(txt){
+		var parrafo = document.createElement("p");
+		parrafo.innerText = txt;
+		var imprimir = document.getElementById("imprimir");
+		imprimir.insertBefore(parrafo, imprimir.childNodes[0]).classList.add("box");
+	};
+	function autosize(){
+ 		var el = this;
+  		setTimeout(function(){
+		    el.style.cssText = 'height:auto; padding:0';
+		    el.style.cssText = 'height:' + el.scrollHeight + 'px';
+  		},0);
+	};
+	function resize() {
+       textArea.style.cssText = "height: auto";
+   	}
+	function cantidadCaracteres(longitud){
 		if(longitud <= caracteres){
 			contador.innerHTML = caracteres - longitud;
 		}else{
@@ -31,12 +46,19 @@ window.addEventListener("load", function(){
 		if(longitud > caracteres){
 			boton.disabled = true;
 		}
+	};
+	function cambioColor(longitud){
 		if(longitud > 120){
-			contador.style.color = "purple";
+			contador.classList.add("purple");
 		}
 		if(longitud > 130){
-			contador.style.color = "orange";
+			contador.classList.add("white");
 		}
-
-	})
+		if(longitud < 120){
+			contador.classList.remove("purple");
+		}
+		if(longitud < 130){
+			contador.classList.remove("white");
+		}
+	};
 });
